b192663586c6b55b881f76c3dd1b1f4e2ca9d34e
yob-Branch <yob.teshome27@gmail.com>
2025-04-09 15:29:19 +0300

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return 'api';
+});
